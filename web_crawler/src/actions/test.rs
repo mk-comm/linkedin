ecadb8c462d015965eea2e37099e167263e642f6
mk-comm <services@kuzmin.email>
2023-05-19 22:23:16 +0200

    Fix

---
+++ web_crawler/src/actions/test.rs
@@ -0,0 +1,24 @@
+///Goals: 
+///Scrap messages - create if new
+///Update entityurn based on LinkedinNick
+///Evaluate conversation - Mark read/unread
+	
+	
+	///show candidate in messaging
+	//Scrap linkedin_nick
+	//Scrap entity_Urn
+	//based on linkedinnick find candidate and update entityurn and used.id
+		//update entityurn for talent and candidate on bubble side
+	//
+	
+	///message scrapping
+	//Scrap the message
+	//Check if message new or not
+	//If yes - create_message, else ()
+	
+	
+	///separate from loop for messages
+	//check if candidate has automation flow based on linkedinnick
+	//if yes evaluate whole text, mark read/unread
+	//if no mark read/unread based on initial state 
+	
